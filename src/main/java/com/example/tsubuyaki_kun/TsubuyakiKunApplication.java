145b1d49f4db9220bdc61431e923a930deba8f0b
ganden <ysk.ganden@gmail.com>
2024-11-04 13:27:47 +0900

    First commit.

---
+++ src/main/java/com/example/tsubuyaki_kun/TsubuyakiKunApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsubuyaki_kun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsubuyakiKunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsubuyakiKunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tsubuyaki_kun/TsubuyakiKunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsubuyaki_kun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsubuyakiKunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
